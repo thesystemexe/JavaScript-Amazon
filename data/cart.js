b8ca32b273c576026d6496af9b5d34e55e56a29e
saurabh <thesystem.exee@gmail.com>
2024-07-29 21:01:27 +0530

    cart related work

---
+++ data/cart.js
@@ -0,0 +1,49 @@
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if(!cart){
+    cart =
+        [
+  {
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 2,
+  },
+  {
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 2,
+  },
+]
+}
+export function addToCart(productId) {
+  let matchingItem;
+  cart.forEach((item) => {
+    if (productId === item.productId) {
+      matchingItem = item;
+    }
+  });
+  if (matchingItem) {
+    matchingItem.quantity += 1;
+  } else {
+    cart.push({
+      productId: productId,
+      quantity: 1,
+    });
+  }
+
+  saveToStorage();
+  console.log(cart);
+}
+
+function saveToStorage() {
+  localStorage("cart", JSON.stringify(cart));
+}
+
+export function removeFromCart(productId) {
+  const newCart = [];
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    }
+  });
+  cart = newCart;
+  saveToStorage();
+}
---
+++ scripts/utils/money.js
@@ -0,0 +1,3 @@
+export function money(priceCents) {
+  return (priceCents / 100).toFixed(2);
+}
